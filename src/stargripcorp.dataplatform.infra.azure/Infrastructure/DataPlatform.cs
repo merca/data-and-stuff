7d104cc1d337a50a9f2142ee135567d3cb854c26
Merca Ovnerud <merca@users.noreply.github.com>
2023-10-01 16:37:51 +0200

    fix missing tag parameter

--- src/stargripcorp.dataplatform.infra.azure/Infrastructure/DataPlatform.cs
+++ src/stargripcorp.dataplatform.infra.azure/Infrastructure/DataPlatform.cs
@@ -9,15 +9,17 @@ internal class DataPlatform
     private readonly StackConfig _config;
     private readonly NamingConvention _naming;
     private readonly string shortName = "data";
+    private readonly Dictionary<string, string> _tags;
 
     public DataPlatform(StackConfig config, NamingConvention naming, Dictionary<string, string> tags)
     {
         _config = config;
         _naming = naming;
+        _tags = tags;
     }
     public void Run()
     {
-        var rg = new AzResourceGroup($"{shortName}-rg", _naming).WithBudget(20, ["merca.ovnerud@pulumi.me"]);
+        var rg = new AzResourceGroup($"{shortName}-rg", _naming, _tags).WithBudget(20, ["merca.ovnerud@pulumi.me"]);
         var storage = new AzStorage($"{shortName}-sa", _naming, rg.ResourceGroupName).AsDataLake();
     }
 }
