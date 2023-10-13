f75bfc58ac5932a3bda1c245350b704e838f8b84
Merca Ovnerud <merca@users.noreply.github.com>
2023-10-13 20:28:50 +0200

    Refactor DataPlatform.cs: Add admins and key vault secret
    - Add `Pulumi` namespace
    - Add `admins` dictionary with two entries
    - Create `AzKeyVault` instance with admins and a secret

--- src/stargripcorp.dataplatform.infra.azure/Infrastructure/DataPlatform.cs
+++ src/stargripcorp.dataplatform.infra.azure/Infrastructure/DataPlatform.cs
@@ -1,4 +1,5 @@
-﻿using stargripcorp.dataplatform.infra.azure.Resources;
+﻿using Pulumi;
+using stargripcorp.dataplatform.infra.azure.Resources;
 using stargripcorp.dataplatform.infra.utils.Naming;
 
 namespace stargripcorp.dataplatform.infra.azure.Infrastructure;
@@ -7,8 +8,15 @@ internal class DataPlatform(NamingConvention naming, Dictionary<string, string> 
 {
     public void Run()
     {
+        var admins = new Dictionary<string, bool>
+        {
+            {"f5d889ea-e64e-467a-9240-f875ff284c04", false}, //deploy-sp
+            {"3a668e53-336b-4d30-94bf-6620cdd036ec", true } //me
+        };
         var shortName = "data";
         var rg = new AzResourceGroup($"{shortName}-rg", naming, tags).WithBudget(20, ["merca.ovnerud@pulumi.me"]);
         _ = new AzStorage($"{shortName}-sa", naming, rg.ResourceGroupName).AsDataLake();
+        _ = new AzKeyVault($"{shortName}-kv", naming, rg.ResourceGroupName, tags)
+            .WithKeyVaultSecretsAdmins(Output.Create(admins)).WithSecret("test", "test");
     }
 }
